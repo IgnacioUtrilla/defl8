5547366c04023acb91f007568e6cffbd682e7a58
ignacioUtrilla <utrillamas@gmail.com>
2021-01-12 23:42:00 +0100

    test: aggiunti unit test per int2bin e bin2int fns

--- lib/lz78/encoding/lz78.test.c
+++ lib/lz78/encoding/lz78.test.c
@@ -39,39 +39,3 @@
 //  size = sizeof(out2) / sizeof(char);
 //  cr_expect_arr_eq(lz78(input).ptr, out2, size, "LONG MESSAGE WITH SAME CHAR");
 //}
-
-Test(LZ78, INT2BIN) {
-  char *dest = (char *) malloc(sizeof(char) * 13); // bit massimi 12 + 1 (\0)
-  int2bin(2, 4, dest);
-  cr_expect_str_eq(dest, "0010");
-
-  strcpy(dest, "");
-  int2bin(0, 4, dest);
-  cr_expect_str_eq(dest, "0000");
-
-  strcpy(dest, "");
-  int2bin(4, 4, dest);
-  cr_expect_str_eq(dest, "0100");
-
-  strcpy(dest, "");
-  int2bin(8, 4, dest);
-  cr_expect_str_eq(dest, "1000");
-
-  strcpy(dest, "");
-  int2bin(13, 4, dest);
-  cr_expect_str_eq(dest, "1101");
-
-  strcpy(dest, "");
-  int2bin(128, 8, dest);
-  cr_expect_str_eq(dest, "10000000");
-
-  strcpy(dest, "");
-  int2bin(255, 8, dest);
-  cr_expect_str_eq(dest, "11111111");
-
-  strcpy(dest, "");
-  int2bin(4095, 12, dest);
-  cr_expect_str_eq(dest, "111111111111");
-
-  free(dest);
-}
